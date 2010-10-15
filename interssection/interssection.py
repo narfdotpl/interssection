4dc8b6452ddbfee9ea9e3f83a46b0453248f85c2
Maciej Konieczny <hello@narf.pl>
2010-10-15 21:03:08 +0200

    added set operations

--- interssection/interssection.py
+++ interssection/interssection.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import datetime
+import uuid
+
 from feedparser import parse
 
 from templates import render_as_atom
@@ -8,6 +11,21 @@ from templates import render_as_atom
 
 __all__ = ['__author__', 'Feed']
 __author__ = 'Maciej Konieczny <hello@narf.pl>'
+
+
+class MetaFeed(type):
+
+    def __new__(cls, class_name, base_classes, attributes_dict):
+        for method_name in ['__and__', '__ge__', '__gt__', '__le__', '__len__',
+                            '__lt__', '__or__', '__sub__', '__xor__',
+                            'difference', 'intersection', 'isdisjoint',
+                            'issubset', 'issuperset', 'symmetric_difference',
+                            'union']:
+            if method_name not in attributes_dict:
+                method = create_set_method(method_name)
+                attributes_dict[method_name] = method
+
+        return type.__new__(cls, class_name, base_classes, attributes_dict)
 
 
 def clear_cache(method):
@@ -22,13 +40,61 @@ def clear_cache(method):
     return decorated_method
 
 
+def create_set_method(method_name):
+    def method(*feeds):
+        # prepare feeds
+        for feed in feeds:
+            if feed._entry_ids is None:
+                feed._entries_by_id = dict((e.id, e) for e in feed._entries)
+                feed._entry_ids = frozenset(feed._entries_by_id.iterkeys())
+
+        # get id sets
+        first_set = feeds[0]._entry_ids
+        other_sets = (feed._entry_ids for feed in feeds[1:])
+
+        # run method
+        result = getattr(first_set, method_name)(*other_sets)
+
+        # return bools, etc.
+        if not isinstance(result, frozenset):
+            return result
+
+        # create resultant feed
+        entries_by_id = {}
+        for feed in feeds:
+            entries_by_id.update(feed._entries_by_id)
+        entries = [entries_by_id[id] for id in result]
+
+        title = (' ' + method_name + ' ').join(feed.title for feed in feeds)
+        return Feed(id=get_urn(), title=title, updated=get_iso8601_datetime(),
+                    author='interssection', entries=entries)
+
+    return method
+
+
 class Feed(object):
 
+    __metaclass__ = MetaFeed
+
     @clear_cache
-    def __init__(self, string_or_url):
+    def __init__(self, string_or_url=None, **feed_attributes):
         """
         Read feed from string or URL.
         """
+
+        # set initial...
+        self._entry_ids = None
+
+        # pretend it's `def __init__(self, string_or_url):`
+        if string_or_url is None:
+            if not feed_attributes:
+                message = '__init__() takes exactly 2 arguments (1 given)'
+                raise TypeError(message)
+
+            # use backdoor
+            for name in ['id', 'title', 'updated', 'author', 'entries']:
+                setattr(self, '_' + name, feed_attributes[name])
+            return
 
         # accept only string (type) argument
         if not isinstance(string_or_url, basestring):
@@ -36,21 +102,26 @@ class Feed(object):
                             .format(string_or_url.__class__.__name__))
 
         # parse feed
-        self._feed = parse(string_or_url)
+        parsed = parse(string_or_url)
 
         # raise exception if it occured during parsing
-        exception = self._feed.get('bozo_exception')
+        exception = parsed.get('bozo_exception')
         if exception:
             raise exception
 
+        # set attributes
+        for name in ['id', 'title', 'updated', 'author']:
+            setattr(self, '_' + name, parsed.feed[name])
+        self._entries = parsed.entries
+
     @property
     def title(self):
-        return self._feed.feed.title
+        return self._title
 
     @title.setter
     @clear_cache
     def title(self, new_title):
-        self._feed.feed.title = new_title
+        self._title = new_title
 
     def __str__(self):
         """
@@ -58,5 +129,18 @@ class Feed(object):
         """
 
         if self._xml is None:
-            self._xml = render_as_atom(self._feed)
+            # pick feed attributes
+            context = {'entries': self._entries}
+            for name in ['id', 'title', 'updated', 'author']:
+                context[name] = getattr(self, '_' + name)
+
+            self._xml = render_as_atom(context)
+
         return self._xml
+
+
+def get_iso8601_datetime():
+    return datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+def get_urn():
+    return uuid.uuid4().urn
--- tests/testsuite.py
+++ tests/testsuite.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import datetime
+
 from feedparser import parse
 from unittest import TestCase, main
 
@@ -58,6 +60,10 @@ class TestInput(TestCase):
         xml = str(Feed(url))
         self.assert_content_is_the_same(atom, xml)
 
+    def test_raise_error_on_no_argument(self):
+        with self.assertRaises(TypeError):
+            Feed()
+
     def test_raise_error_on_incorrect_argument(self):
         for item in [0, 2.72, ('foo',), ['bar'], {'baz': 'quak'}]:
             with self.assertRaises(TypeError):
@@ -70,17 +76,39 @@ class TestInput(TestCase):
 class TestOutput(TestCase):
 
     def test_print_as_valid_feed(self):
-        xml = str(Feed(atom))
-        validate(xml)
+        feed12 = Feed(atom12)
+        feed23 = Feed(atom23)
+        validate(str(feed12))
+        validate(str(feed12 & feed23))
 
     def test_give_unique_id_to_resultant_feed(self):
-        self.fail()
+        feed12 = Feed(atom12)
+        feed23 = Feed(atom23)
+        id1 = parse(str(feed12 & feed23)).feed.id
+        id2 = parse(str(feed12 & feed23)).feed.id
+        self.assertNotEqual(id1, id2)
+
+    def test_use_original_feed_titles_in_resultant_feed_title(self):
+        feed12 = Feed(atom12)
+        feed23 = Feed(atom23)
+        get_title = lambda feed: parse(str(feed)).feed.title
+        title = get_title(feed12 & feed23)
+        title12 = get_title(feed12)
+        title23 = get_title(feed23)
+        self.assertIn(title12, title)
+        self.assertIn(title23, title)
 
     def test_set_current_date_in_resultant_feed(self):
-        self.fail()
+        then = datetime.datetime.utcnow().timetuple()[:6]
+        feed = Feed(atom12) & Feed(atom23)
+        now = datetime.datetime.utcnow().timetuple()[:6]
+        parsed_time = parse(str(feed)).feed.updated_parsed[:6]
+        self.assertTrue(then <= parsed_time <= now)
 
     def test_set_interssection_as_author_of_resultant_feed(self):
-        self.fail()
+        feed = Feed(atom12) & Feed(atom23)
+        parsed = parse(str(feed))
+        self.assertEqual(parsed.feed.author, 'interssection')
 
 
 class TestAttributes(TestCase):
@@ -100,11 +128,137 @@ class TestAttributes(TestCase):
 
 class TestSetOperations(TestCase):
 
-    def test_support_x(self):
-        self.fail()
+    def setUp(self):
+        self.feed12 = Feed(atom12)
+        self.feed23 = Feed(atom23)
+        self.feed34 = Feed(atom34)
 
-    def test_dont_support_y(self):
-        self.fail()
+
+    def test_support___and__(self):
+        feed = self.feed12 & self.feed23
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 1)
+        self.assertEqual(parsed.entries[0].title, '2')
+
+        feed = self.feed12 & self.feed23 & self.feed34
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 0)
+
+    def test_support__ge__(self):
+        self.assertTrue(self.feed12 >= self.feed12)
+        self.assertTrue((self.feed12 | self.feed23) >= self.feed12)
+        self.assertFalse(self.feed12 >= (self.feed12 | self.feed23))
+
+    def test_support__gt__(self):
+        self.assertFalse(self.feed12 > self.feed12)
+        self.assertTrue((self.feed12 | self.feed23) > self.feed12)
+        self.assertFalse(self.feed12 > (self.feed12 | self.feed23))
+
+    def test_support__le__(self):
+        self.assertTrue(self.feed12 <= self.feed12)
+        self.assertTrue(self.feed12 <= self.feed12 | self.feed23)
+        self.assertFalse(self.feed12 <= self.feed23)
+
+    def test_support___len__(self):
+        self.assertEqual(len(self.feed12), 2)
+        self.assertEqual(len(self.feed12 & self.feed23), 1)
+
+    def test_support__lt__(self):
+        self.assertFalse(self.feed12 < self.feed12)
+        self.assertTrue(self.feed12 < self.feed12 | self.feed23)
+        self.assertFalse(self.feed12 < self.feed23)
+
+    def test_support___or__(self):
+        feed = self.feed12 | self.feed23
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 3)
+        self.assertItemsEqual([entry.title for entry in parsed.entries],
+                              ['1', '2', '3'])
+
+        feed = self.feed12 | self.feed23 | self.feed34
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 4)
+        self.assertItemsEqual([entry.title for entry in parsed.entries],
+                              ['1', '2', '3', '4'])
+
+    def test_support___sub__(self):
+        feed = self.feed12 - self.feed23
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 1)
+        self.assertEqual(parsed.entries[0].title, '1')
+
+        feed = self.feed23 - self.feed12 - self.feed34
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 0)
+
+    def test_support___xor__(self):
+        feed = self.feed12 ^ self.feed23
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 2)
+        self.assertItemsEqual([entry.title for entry in parsed.entries],
+                              ['1', '3'])
+
+    def test_support_difference(self):
+        feed = self.feed12.difference(self.feed23)
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 1)
+        self.assertEqual(parsed.entries[0].title, '1')
+
+        feed = self.feed23.difference(self.feed12, self.feed34)
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 0)
+
+    def test_support_intersection(self):
+        feed = self.feed12.intersection(self.feed23)
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 1)
+        self.assertEqual(parsed.entries[0].title, '2')
+
+        feed = self.feed12.intersection(self.feed23, self.feed34)
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 0)
+
+    def test_support_isdisjoint(self):
+        self.assertFalse(self.feed12.isdisjoint(self.feed23))
+        self.assertTrue(self.feed12.isdisjoint(self.feed34))
+
+    def test_support_issubset(self):
+        self.assertTrue(self.feed12.issubset(self.feed12))
+        self.assertTrue(self.feed12.issubset(self.feed12 | self.feed23))
+        self.assertFalse(self.feed12.issubset(self.feed23))
+
+    def test_support_issuperset(self):
+        self.assertTrue(self.feed12.issuperset(self.feed12))
+        self.assertTrue((self.feed12 | self.feed23).issuperset(self.feed12))
+        self.assertFalse(self.feed12.issuperset(self.feed12 | self.feed23))
+
+    def test_support_symmetric_difference(self):
+        feed = self.feed12.symmetric_difference(self.feed23)
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 2)
+        self.assertItemsEqual([entry.title for entry in parsed.entries],
+                              ['1', '3'])
+
+    def test_support_union(self):
+        feed = self.feed12.union(self.feed23)
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 3)
+        self.assertItemsEqual([entry.title for entry in parsed.entries],
+                              ['1', '2', '3'])
+
+        feed = self.feed12.union(self.feed23, self.feed34)
+        parsed = parse(str(feed))
+        self.assertEqual(len(parsed.entries), 4)
+        self.assertItemsEqual([entry.title for entry in parsed.entries],
+                              ['1', '2', '3', '4'])
+
+    def test_dont_support___contains__(self):
+        with self.assertRaises(TypeError):
+            'foo' in Feed(atom)
+
+    def test_dont_support_copy(self):
+        with self.assertRaises(AttributeError):
+            Feed(atom).copy()
 
 
 class TestEntrails(TestCase):
