6f5d44288fc7cc415bb7324e26b04b5fd3429931
Maciej Konieczny <hello@narf.pl>
2010-10-27 00:37:22 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name='interssection',
+    version='0.1.0dev',
+    description='treat Atom and RSS feeds like sets',
+    author='Maciej Konieczny',
+    author_email='hello@narf.pl',
+    url='http://github.com/narfdotpl/interssection',
+    packages=['interssection', 'interssection.templates'],
+    package_data={'interssection.templates': ['*.xml']},
+    install_requires=['Jinja2==2.5.2', 'feedparser==4.1']
+)
